0cf532bd2f189b7b710b92db1549190874eaa898
Nelson Kopliku <nelson.kopliku@suse.com>
2025-11-28 12:07:09 +0100

    fix get_version_from_git.sh

--- hack/get_version_from_git.sh
+++ hack/get_version_from_git.sh
@@ -3,25 +3,23 @@
 # Copyright 2025 SUSE LLC
 # SPDX-License-Identifier: Apache-2.0
 
-set -euo pipefail
+set -e
+set -o pipefail
 
 # https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string
 SEMVER_REGEX="^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$"
 
-TAG=$(git tag 2>/dev/null | grep -P "$SEMVER_REGEX" | sed '/-/!{s/$/_/}' | sort -V | sed 's/_$//' | tail -n1 || true)
+TAG=$(git tag | grep -P "$SEMVER_REGEX" | sed '/-/!{s/$/_/}' | sort -V | sed 's/_$//' | tail -n1)
 
 if [ -z "${TAG}" ]; then
-  # Fall back to VERSION file if no tags found
-  if [ -f "VERSION" ]; then
-    cat VERSION
-  else
-    echo "devel"
-  fi
+  echo "Could not find any semver tag" 1>&2
+  exit 1
 else
   COMMITS_SINCE_TAG=$(git rev-list "${TAG}".. --count)
   if [ "${COMMITS_SINCE_TAG}" -gt 0 ]; then
     COMMIT_INFO=$(git show -s --format=%ct.%h HEAD)
     SUFFIX="+git.${COMMITS_SINCE_TAG}.${COMMIT_INFO}"
   fi
-  echo "${TAG}${SUFFIX}"
 fi
+
+echo "${TAG}${SUFFIX}"
